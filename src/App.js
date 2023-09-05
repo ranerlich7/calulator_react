a9e15fa1d717b1942ec15ab275dfb7f5c6b875bb
ran <ran@goz.com>
2023-09-05 10:33:33 +0300

    added basic computer with +, equals

--- src/App.js
+++ src/App.js
@@ -1,43 +1,83 @@
+import { useEffect, useState } from "react";
 import "./calc.css";
+import Button from "./components/Button";
 
 function App() {
+  const [displayText, setDisplayText] = useState(0);
+  const [memory, setMemory] = useState(0);
+  const [action, setAction] = useState("");
+
+  useEffect(() => {
+    console.log("Updated dispText=", displayText, ". Updated memory=", memory);
+  }, [displayText, memory]);
+
+  function myplus() {
+    console.log("plus pressed");
+    setAction("+");
+    setMemory(memory + Number(displayText));
+    setDisplayText(0);
+  }
+
+  function dotclick() {
+    console.log("dotclick pressed");
+  }
+
+  function calcEquals() {
+    if (action === "+") {
+      setDisplayText(memory + Number(displayText));
+    }
+    setAction("=");
+    setMemory(0);
+  }
+
+  function myclear() {
+    console.log("clear pressed");
+    setDisplayText(0);
+    setMemory(0);
+  }
+  function myclick(e) {
+    if (displayText === 0 || action === "=") {
+      setDisplayText(e.target.innerText);
+      action === "=" && setAction("");
+    } else {
+      setDisplayText(displayText + e.target.innerText);
+    }
+    console.log("dispText=", displayText);
+    // setAction("click");
+  }
+
   return (
     <>
-      <div class="calc-body">
+      <div className="calc-body">
         {/* <!-- Calculator Display Screen --> */}
-        <div class="calc-screen">
+        <div className="calc-screen">
           <div id="calc-operation"></div>
-          <div id="calc-typed">0</div>
+          <div id="calc-typed">{displayText}</div>
         </div>
 
         {/* <!-- Calculator Buttons --> */}
         <div class="calc-button-row">
-          <button onclick="myclear()" class="ac">
-            AC
-          </button>
-          <button class="opt">&#43;&#47;&#8722;</button>
-          <button class="opt">&#37;</button>
-          <button class="opt">&#247;</button>
-          <button onclick="myclick()">7</button>
-          <button onclick="myclick()">8</button>
-          <button onclick="myclick()">9</button>
-          <button class="opt">&#215;</button>
-          <button onclick="myclick()">4</button>
-          <button onclick="myclick()">5</button>
-          <button onclick="myclick()">6</button>
-          <button class="opt">&#8722;</button>
-          <button onclick="myclick()">1</button>
-          <button onclick="myclick()">2</button>
-          <button onclick="myclick()">3</button>
-          <button onclick="myplus()" class="opt">
-            +
-          </button>
-          <button onclick="myclick()">0</button>
-          <button onclick="dotclick()">.</button>
-          <button>&#9003;</button>
-          <button onclick="calcEquals()" class="opt">
-            =
-          </button>
+          <Button buttonClass="ac" buttonText="AC" clickFunction={myclear} />
+          <Button buttonClass="opt" buttonText="+/-" />
+          <Button buttonClass="opt" buttonText="%" />
+          <Button buttonClass="opt" buttonText="/" />
+          <Button clickFunction={myclick} buttonText="7" />
+          <Button clickFunction={myclick} buttonText="8" />
+          <Button clickFunction={myclick} buttonText="9" />
+          <Button buttonClass="opt" buttonText="X" />
+          <Button clickFunction={myclick} buttonText="4" />
+          <Button clickFunction={myclick} buttonText="5" />
+          <Button clickFunction={myclick} buttonText="6" />
+          <Button buttonClass="opt" buttonText="-" />
+          <Button clickFunction={myclick} buttonText="1" />
+          <Button clickFunction={myclick} buttonText="2" />
+          <Button clickFunction={myclick} buttonText="3" />
+          <Button clickFunction={myplus} buttonClass="opt" buttonText="+" />
+
+          <Button clickFunction={myclick} buttonText="0" />
+          <Button clickFunction={dotclick} buttonText="." />
+          <Button buttonText="&#9003;" />
+          <Button clickFunction={calcEquals} buttonClass="opt" buttonText="=" />
         </div>
       </div>
     </>
