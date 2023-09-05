b84449af99b5d69616191a4c844af1fe3393458d
ran <ran@goz.com>
2023-09-05 09:07:18 +0300

    added calculator basic app without logic only design

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./calc.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div class="calc-body">
+        {/* <!-- Calculator Display Screen --> */}
+        <div class="calc-screen">
+          <div id="calc-operation"></div>
+          <div id="calc-typed">0</div>
+        </div>
+
+        {/* <!-- Calculator Buttons --> */}
+        <div class="calc-button-row">
+          <button onclick="myclear()" class="ac">
+            AC
+          </button>
+          <button class="opt">&#43;&#47;&#8722;</button>
+          <button class="opt">&#37;</button>
+          <button class="opt">&#247;</button>
+          <button onclick="myclick()">7</button>
+          <button onclick="myclick()">8</button>
+          <button onclick="myclick()">9</button>
+          <button class="opt">&#215;</button>
+          <button onclick="myclick()">4</button>
+          <button onclick="myclick()">5</button>
+          <button onclick="myclick()">6</button>
+          <button class="opt">&#8722;</button>
+          <button onclick="myclick()">1</button>
+          <button onclick="myclick()">2</button>
+          <button onclick="myclick()">3</button>
+          <button onclick="myplus()" class="opt">
+            +
+          </button>
+          <button onclick="myclick()">0</button>
+          <button onclick="dotclick()">.</button>
+          <button>&#9003;</button>
+          <button onclick="calcEquals()" class="opt">
+            =
+          </button>
+        </div>
+      </div>
+    </>
   );
 }
 
